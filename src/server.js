f59905858b0917e5f46ddd209bbe48d11193095a
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2023-06-15 20:07:01 +1000

    database connected

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
-
+const mongoose = require('mongoose')
 const app = express();
 
 app.use(express.json());
+
+async function dbConnect(){
+	try{
+		await mongoose.connect('mongodb://localhost:27017/note_taking_db')
+		console.log("Database connected!")
+	} catch (error) {
+		console.log(`dbConnect failed, error: ${JSON.stringify(error)}`)
+	}
+}
+
+dbConnect()
 
 app.get("/", (request, response) => {
 	response.json({
