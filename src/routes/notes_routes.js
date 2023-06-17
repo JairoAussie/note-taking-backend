5e95a0653bfd2a4dab6081b09d6d21cd600a2ba4
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2023-06-17 10:22:25 +1000

    find by id and delete note

--- src/routes/notes_routes.js
+++ src/routes/notes_routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const notesRouter = express.Router();
-const {getNotes, createNote, deleteAllNotes} = require('../controllers/notes_controller')
+const {getNotes, getNote, createNote, deleteAllNotes, deleteNote} = require('../controllers/notes_controller')
 
 notesRouter.get("/", getNotes)
+
+notesRouter.get("/:id", getNote)
 
 notesRouter.post("/", createNote)
 
 notesRouter.delete("/clear", deleteAllNotes)
 
+notesRouter.delete("/:id", deleteNote)
 module.exports = notesRouter
