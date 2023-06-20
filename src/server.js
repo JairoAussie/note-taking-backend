d26e872d06b5b43a18c54df24857908459f07fee
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2023-06-20 19:39:43 +1000

    server.js has cors and helmet and 404message

--- src/server.js
+++ src/server.js
@@ -18,7 +18,16 @@ app.use(helmet.contentSecurityPolicy({
 	}
 }))
 
+const cors = require('cors')
+let corsOptions = {
+	origin: ["http://localhost:3000"],
+	optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 async function dbConnect(){
 	try{
@@ -42,6 +51,14 @@ app.use("/notes", notesRouter)
 
 const usersRouter = require('./routes/users_routes')
 app.use("/users", usersRouter)
+
+app.get('*', (request, response) =>{
+	response.status(404)
+	response.json({
+		message: "Route not found",
+		path: request.path
+	})
+})
 
 module.exports = {
 	app, HOST, PORT
